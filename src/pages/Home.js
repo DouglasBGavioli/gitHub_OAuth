69d2254514abd9dc132674323aa0125d1364c8bc
DouglasGavioli <douglasbgavioli@hotmail.com>
2022-03-15 14:36:48 -0300

    confirmar main

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -59,67 +59,85 @@ function Home() {
 
     return (
         <>
-            <h3 className={styles.user}>Bem-vindo: {userLogin} </h3>
-            <section className={styles.home_container}>
-                <form className={styles.form_card} onSubmit={onSubmit}>
-                    <Input
-                        type="text"
-                        text="Buscar"
-                        placeholder="Digite nome de usuario"
-                        handleOnChange={handleInput}
-                    />
-                    <button className={styles.buttons} onClick={clickRepos}>
-                        Repositorios
-                    </button>
-                    <button className={styles.buttons} onClick={clickStarred}>
-                        Starred
-                    </button>
-                </form>
-            </section>
+            {userLogin ? (
+                <>
+                    <h3 className={styles.user}>Bem-vindo: {userLogin} </h3>
+                    <section className={styles.home_container}>
+                        <form className={styles.form_card} onSubmit={onSubmit}>
+                            <Input
+                                type="text"
+                                text="Buscar"
+                                placeholder="Digite nome de usuario"
+                                handleOnChange={handleInput}
+                            />
+                            <button
+                                className={styles.buttons}
+                                onClick={clickRepos}
+                            >
+                                Repositorios
+                            </button>
+                            <button
+                                className={styles.buttons}
+                                onClick={clickStarred}
+                            >
+                                Starred
+                            </button>
+                        </form>
+                    </section>
 
-            {!hideStarred &&
-                starred.map((starr) => (
-                    <div className={styles.homeCard} key={starr.id}>
-                        <img
-                            className={styles.avatar}
-                            alt="avatar"
-                            src={starr.owner.avatar_url}
-                        />
-                        <h4>Postado por: {starr.owner.login}</h4>
-                        <div className={styles.homeCardDown}>
-                            {starr.description ? (
-                                <p>{starr.description}</p>
-                            ) : (
-                                <p>Sem descrição</p>
-                            )}
-                        </div>
-                        <div className={styles.link}>
-                            <a href={starr.html_url}>Link:{starr.html_url}</a>
-                        </div>
-                        <div className={styles.data}>
-                            <p> {starr.created_at.substring(0, 10)}</p>
-                        </div>
-                    </div>
-                ))}
-            {!hideRepos &&
-                repos.map((rep) => (
-                    <div className={styles.homeCard} key={rep.id}>
-                        <h4>Postado por: {rep.owner.login}</h4>
-                        <div className={styles.homeCardDown}>
-                            {rep.description ? (
-                                <p>{rep.description}</p>
-                            ) : (
-                                <p>Sem descrição</p>
-                            )}
-                        </div>
-                        <div className={styles.link}>
-                            <a href={rep.html_url}>Link:{rep.html_url}</a>
-                        </div>
-                        <div className={styles.data}>
-                            <p> {rep.created_at.substring(0, 10)}</p>
-                        </div>
-                    </div>
-                ))}
+                    {!hideStarred &&
+                        starred.map((starr) => (
+                            <div className={styles.homeCard} key={starr.id}>
+                                <img
+                                    className={styles.avatar}
+                                    alt="avatar"
+                                    src={starr.owner.avatar_url}
+                                />
+                                <h4>Postado por: {starr.owner.login}</h4>
+
+                                <div className={styles.homeCardDown}>
+                                    <h4>Repositorio: {starr.name}</h4>
+                                    {starr.description ? (
+                                        <p>Descrição: {starr.description}</p>
+                                    ) : (
+                                        <p>Sem descrição</p>
+                                    )}
+                                </div>
+                                <div className={styles.link}>
+                                    <a href={starr.html_url}>
+                                        Link:{starr.html_url}
+                                    </a>
+                                </div>
+                                <div className={styles.data}>
+                                    <p> {starr.created_at.substring(0, 10)}</p>
+                                </div>
+                            </div>
+                        ))}
+                    {!hideRepos &&
+                        repos.map((rep) => (
+                            <div className={styles.homeCard} key={rep.id}>
+                                <h4>Repositorio: {rep.name}</h4>
+                                <div className={styles.homeCardDown}>
+                                    {rep.description ? (
+                                        <p>Descrição: {rep.description}</p>
+                                    ) : (
+                                        <p>Sem descrição</p>
+                                    )}
+                                </div>
+                                <div className={styles.link}>
+                                    <a href={rep.html_url}>
+                                        Link:{rep.html_url}
+                                    </a>
+                                </div>
+                                <div className={styles.data}>
+                                    <p> {rep.created_at.substring(0, 10)}</p>
+                                </div>
+                            </div>
+                        ))}
+                </>
+            ) : (
+                <span>Você deve logar primeiro</span>
+            )}
         </>
     );
 }
